c6b84f05316b19c08b290328fac779a1b6f5239c
Jemima Abu <jemimaabu@gmail.com>
2019-08-31 14:39:26 +0100

    Included fetch request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component  {
+class App extends React.Component {
   constructor(props) {
     super(props);
 
@@ -11,31 +11,23 @@ class App extends React.Component  {
       quotes: []
     };
   };
-  
+
   componentDidMount() {
     this.getQuotes();
-    
   }
-  
+
   getQuotes() {
     var self = this;
-    return $.ajax({
-      headers: {
-        Accept: "application/json"
-      },
-      url: 'https://gist.githubusercontent.com/jemimaabu/07d9e9d7e7fc0b5f7008d0fc462ff9c2/raw/efe2e9cbb0fba82c4c216ba2b8d7c273af4324cf/literary-sexts.json',
-      success: function(jsonQuotes) {
-        if (typeof jsonQuotes === 'string') {
-          var quotesData = JSON.parse(jsonQuotes);
-          self.setState({
-            quotes: quotesData.quotes
-          })
-          self.getRandomQuote();
-        }
-      }
-    });
+    fetch('https://gist.githubusercontent.com/jemimaabu/07d9e9d7e7fc0b5f7008d0fc462ff9c2/raw/efe2e9cbb0fba82c4c216ba2b8d7c273af4324cf/literary-sexts.json')
+      .then(function (response) { return response.json(); })
+      .then(function (quotesData) {
+        self.setState({
+          quotes: quotesData.quotes
+        })
+        self.getRandomQuote();
+      });
   }
-  
+
   getRandomQuote() {
     var quotes = this.state.quotes;
     var i = Math.floor(Math.random() * quotes.length);
@@ -44,9 +36,9 @@ class App extends React.Component  {
       author: quotes[i]["source"].trim()
     })
   }
- 
+
   render() {
-    const { text, author } = this.state; 
+    const { text, author } = this.state;
     return (
       <main id="quote-component">
         <header>
@@ -54,14 +46,14 @@ class App extends React.Component  {
           <p>A collection of quotes from <a href="https://www.tumblr.com/search/literary+sexts" target="_blank">tumblr.com</a></p>
         </header>
         <div id="quote-box">
-          <q 
-            id="text" 
-            dangerouslySetInnerHTML={{__html: text}} />
-          <p 
+          <q
+            id="text"
+            dangerouslySetInnerHTML={{ __html: text }} />
+          <p
             id="author"
-            dangerouslySetInnerHTML={{__html: author}} />
-          <button onClick = {() => this.getRandomQuote()} id="new-quote">New quote</button>
-          <a href={`https://twitter.com/intent/tweet?text=${encodeURI("\"" + text + "\""+ "\n \n" + "https://codepen.io/Jemimaabu/pen/bGbRzea")}`} id="tweet-quote" target="_blank">
+            dangerouslySetInnerHTML={{ __html: author }} />
+          <button onClick={() => this.getRandomQuote()} id="new-quote">New quote</button>
+          <a href={`https://twitter.com/intent/tweet?text=${encodeURI("\"" + text + "\"" + "\n \n" + "https://codepen.io/Jemimaabu/pen/bGbRzea")}`} id="tweet-quote" target="_blank">
             <small>Tweet</small>
           </a>
         </div>
